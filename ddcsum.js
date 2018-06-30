66c5c063097d1c94b210e1fd2bd62e4c77cb4a46
Ronald Casili <nvlled@gmail.com>
2018-06-30 20:13:03 +0800

    Implement generateCallNumber function

--- ddcsum.js
+++ ddcsum.js
@@ -3991,8 +3991,53 @@ const data = [
         "classNum": "999",
         "heading": "Extraterrestrial worlds"
     }
-]
+];
+
+const generateCutter = require("cuttersanborn");
+
+const util = {
+    firstLowerLetter(str) {
+        if (!str || !str.trim())
+            return "";
+        return str[0].toLowerCase();
+    },
+}
+
+function generateCallNumber(args) {
+    let {
+        author,
+        biographySubject,
+        title,
+        classNumber,
+        publishYear,
+        copyNumber,
+    } = args;
+    let fields = [classNumber];
+
+    let {firstname: afname, lastname: alname} = author || {};
+    let suf = util.firstLowerLetter(title);
+    if (biographySubject) {
+        let {firstname: bfname, lastname: blname} = biographySubject || {};
+        fields = fields.concat([
+            generateCutter(blname, bfname) + suf,
+            generateCutter(alname, afname),
+        ]);
+    } else if (afname && alname) {
+        fields = fields.concat([
+            generateCutter(alname, afname)+suf,
+        ]);
+    }
+    if (!isNaN(+copyNumber)) {
+        fields.push(`c.${copyNumber}`);
+    }
+    if (publishYear) {
+        fields.push(publishYear);
+    }
+
+    return fields.filter(o=>o).join(" ");
+}
 
 module.exports = {
     summary: data,
+    generateCallNumber,
 }
